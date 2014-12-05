21ff7621eda76456fed79c699ffb79d75293f572
Deepak <deepakn@mit.edu>
2014-12-04 21:03:55 -0500

    Changes to paper

--- project3/code/compute_epsilon_estimate.py
+++ project3/code/compute_epsilon_estimate.py
@@ -24,26 +24,37 @@ def compute_dp(vector1, vector2):
 
 
 def compute_epsilon_random(n, N):
-     random_vectors = get_random_vectors(n, N)
+     # random_vectors = get_random_vectors(n, N)
+     # a = 0.85
+     # b = 0.53
+     a = 0.53
+     b = 0.85
+     random_vectors = [[a, b, 0.0], [a, -b, 0], [b, 0, a], [b, 0.0, -a], [0, a, b], [0, -a, b]]
      max_dp = 0.0
      for i in xrange(len(random_vectors)):
          for j in xrange(i+1, len(random_vectors)):
              dp = compute_dp(random_vectors[i], random_vectors[j])
              if max_dp < abs(dp):
                  max_dp = abs(dp)
-     return max_dp
+     return max_dp, random_vectors
 
 
 def get_min_epsilon(n, N, num_iter):
     min_epsilon = 1.0
+    best_vectors = None
     for i in xrange(num_iter):
-        #if i%10000==0: print i
-        epsilon = compute_epsilon_random(n, N)
+        if i % 10 == 0:
+            print "Done with iteration %d..." % i
+            print "Minimum epsilon seen so far %.2f..." % min_epsilon ** 2
+            print "Best vectors so far:", best_vectors
+            print
+        epsilon, random_vectors = compute_epsilon_random(n, N)
         if epsilon < min_epsilon:
             min_epsilon = epsilon
+            best_vectors = random_vectors
     print min_epsilon ** 2
 
 
 if __name__ == '__main__':
-    get_min_epsilon(3, 6, 1000000)
-        
+    get_min_epsilon(3, 6, 100)
+ 
